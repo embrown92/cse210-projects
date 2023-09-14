cf3c922b7f6bfc5586fdabd3fc446afca8f89c9c
Eda Mae Brown <eda_awe92@yahoo.com.ph>
2023-09-14 11:41:35 -0400

    Finished C# Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your total score? ");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+
+        if (number >= 90)
+        {
+            Console.WriteLine("Congratulations, you passed and got an A!");
+        }
+        else if (number >= 80)
+        {
+            Console.WriteLine("Congratulations, you passed and got a B!");
+        }
+        else if (number >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed and got a C!");
+        }
+        else if (number >= 60)
+        {
+            Console.WriteLine("You got a D. Try again next time.");
+        }
+        else
+        {
+            Console.WriteLine("You got an F. Try again next time.");
+        }
     }
 }
