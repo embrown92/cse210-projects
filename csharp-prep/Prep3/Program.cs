87254318040015a7e35a9a73212d952889ae729c
Eda Mae Brown <eda_awe92@yahoo.com.ph>
2023-09-14 13:34:18 -0400

    Finished Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,31 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Console.Write("Guess the magic number between 1 and 100.");
+        int magicNumber = 6;
+
+        int guessNumber = -1;
+
+        while (guessNumber != magicNumber)
+        {
+            Console.Write(" What is your guess? ");
+            guessNumber = int.Parse(Console.ReadLine());
+
+            if (guessNumber < 6)
+            {
+                Console.WriteLine("Higher!");
+            }
+            else if (guessNumber > 6)
+            {
+                Console.WriteLine("Lower!");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
